130fe0fdbd3137bf065fc39864c037c758a625a1
ankit <sahuankit.murhu@gmail.com>
2024-09-01 10:45:45 +0530

    github copilot

--- dotfiles/nvim/lua/configs/conform.lua
+++ dotfiles/nvim/lua/configs/conform.lua
@@ -3,6 +3,8 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
+    rust = { "rustfmt" },
+    typescript = { "prettier" },
   },
 
   format_on_save = {
--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -8,3 +8,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- map("i", "<C-Tab>", function()
+--   require("copilot.suggestion").accept()
+-- end, { desc = "Copilot accept" })
+
+map('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
