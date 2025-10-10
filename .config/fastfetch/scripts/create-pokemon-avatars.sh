9904296caeb993ec73def9d2666bc0c5b5beafb7
ankit <sahuankit.murhu@gmail.com>
2025-10-10 09:19:58 +0530

    feat: fastfetch random pokemon avatars generator

---
+++ .config/fastfetch/scripts/create-pokemon-avatars.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+main() {
+  cd ../logos/ 
+  mkdir -p pokemons
+  declare -A saved
+  read -p "how many pokemons: " pokemon_count
+
+  for i in $(seq 1 $pokemon_count); do
+    temp=$(mktemp)
+    pokemonthink hi > "$temp"
+
+    # get Pokémon name (last non-empty line)
+    name=$(awk 'NF {last=$0} END {print last}' "$temp")
+
+    # skip if already saved
+    if [[ ${saved[$name]} ]]; then
+      rm "$temp"
+      continue
+    fi
+
+    
+    # Find the line number Pokemon art starts
+    start=$(awk 'NR>1 {if (p ~ /o/) last=NR-1} {p=$0} END{print last}' "$temp")
+    start=$((start + 1))
+    end=$(wc -l < $temp)
+    end=$((end - 2))
+    area=$((end - start + 1))
+
+    #Extract the pokemon art
+    if [[ -n "$start" ]]; then
+      cat "$temp" | head -n $end | tail -n $area > "pokemons/${name}.txt"
+      saved[$name]=1
+      echo "Saved $name"
+    fi
+    rm "$temp"
+  done
+}
+
+main
